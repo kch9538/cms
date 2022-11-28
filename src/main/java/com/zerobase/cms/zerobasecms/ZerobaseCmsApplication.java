65902463f006dd215069602f27bb40725eedff65
kch9538 <108704841+kch9538@users.noreply.github.com>
2022-11-28 14:29:44 +0900

    [007-INIT-ORDER-API] order-api 초기 세팅

--- src/main/java/com/zerobase/cms/zerobasecms/ZerobaseCmsApplication.java
+++ src/main/java/com/zerobase/cms/zerobasecms/ZerobaseCmsApplication.java
@@ -1,12 +1,7 @@
 package com.zerobase.cms.zerobasecms;
 
-import lombok.RequiredArgsConstructor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.ServletComponentScan;
-import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 
 @SpringBootApplication
